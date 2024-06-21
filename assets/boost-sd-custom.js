769a7c5341022ee2ad761c2325f8cb4016be34f1
azeem-rainycity <muhammad.azeem@rainycityagency.com>
2024-06-21 11:05:57 +0100

    product cards amends for collection pages

--- assets/boost-sd-custom.js
+++ assets/boost-sd-custom.js
@@ -25,6 +25,17 @@ const customize = {
                      //    }
                      // }
                       /* End example */
+                      const tags_have = productData.tags;
+                      if(productData.product_type == 'Lighting' ||  productData.product_type == 'Furniture' || productData.product_type == 'Carpets' ||  tags_have.includes('carpets') || productData.product_type == 'Carpet')
+        {
+        const price_widget = `<p class="login_for_pricing"><a class="login" href="/account/login">Log in</a> <span>for pricing</span></p>`;
+        productItem.querySelector('.boost-sd__product-info').insertAdjacentHTML('beforeend', price_widget);
+        }
+      else{
+         console.log('yes here is');
+        const price_widget = `<p class="login_for_pricing"><a class="login" href="/account/login">Log in</a> <span>for pricing and samples</span></p>`;
+      }
+                      
                   }
                   // End-------------------------------------------------------------
                } catch (e) {
